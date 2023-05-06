59833f7578cbbcc3d257e12ad6969e661a2b9c86
Ivan Afanasev <diykarelia@gmail.com>
2023-05-06 13:32:22 +0200

    supabase service working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,49 +1,26 @@
-import React, {useEffect, useState} from 'react';
+// ⛔️⛔️⛔️ The commented lines in this component are just an example of how to render data from Supabase
+// import { useEffect, useState } from 'react';
 import './App.css';
 import Home from './Components/home/Home';
-import { createClient } from "@supabase/supabase-js";
-import { Database } from './types/supabase'
-import {User} from './types/user.type';
-import { PostgrestSingleResponse } from '@supabase/supabase-js';
+// import { User } from './types/user.type';
+// import { supabase } from './services/supabase.service';
 
-const supabase = createClient<Database>(process.env.REACT_APP_SUPABASE_URL!, process.env.REACT_APP_SUPABASE_ANON_KEY!,);
+const App: React.FC = (): JSX.Element => {
+  // const [users, setUsers] = useState<User[]>();
 
+  // useEffect(() => {
+  //   supabase.getUsers().then((users) => setUsers(users));
+  // }, []);
 
-// type UsersResponse = Awaited<ReturnType<typeof getUsers>>
-// export type UsersResponseSuccess = UsersResponse['data']
-// export type UsersResponseError = UsersResponse['error']
-
-const App: React.FC = ():JSX.Element => {
-  const [users, setUsers] = useState<User[]>()
-  
-  async function getUsers() {
-    
-    let data = await supabase.from('Users').select()
-    console.log("hello")
-    if (data && data.data) {
-    setUsers(data.data);
-    console.log(data)
-    }
-  }
-  
-
-  
-  useEffect(() => {
-    getUsers();
-  }, []);
-  
-  
   return (
     <>
-    <ul>
-      
-        {users && users.map((user) => (
-          <li key={user.first_name}>{user.last_name}</li>
-        ))}
-      </ul>
-    <Home/>
+      {/* <ul>
+        {users &&
+          users.map((user) => <li key={user.first_name}>{user.last_name}</li>)}
+      </ul> */}
+      <Home />
     </>
   );
-}
+};
 
 export default App;
