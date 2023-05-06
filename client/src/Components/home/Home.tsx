ee39320ed8d839d3806f61764ef99bc439bc150e
Raul Barros <raulhbarros@outlook.com>
2023-05-06 11:49:21 +0200

    feat profile barebones done

--- client/src/Components/home/Home.tsx
+++ client/src/Components/home/Home.tsx
@@ -5,20 +5,20 @@ import GearListings from '../gear/GearListings';
 import Login from '../auth/Login';
 import Signup from '../auth/Signup';
 import AddGear from '../gear/AddGear';
-
+import UserProfile from '../users/UserProfile';
 const Home: React.FC = (): JSX.Element => {
   return (
     <>
       <div className="component-container">
         <h1>Home</h1>
-         <NavBar></NavBar>
+        <NavBar></NavBar>
         <CurrentRental></CurrentRental>
         {/* <GearListings></GearListings>  */}
         <Login></Login>
         <Signup></Signup>
         <AddGear></AddGear>
+        <UserProfile></UserProfile>
       </div>
-
     </>
   );
 };
