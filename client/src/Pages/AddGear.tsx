33f6ecc86dc43c9eb98afe03bc633c8cbd8e3d6b
Ivan Afanasev <diykarelia@gmail.com>
2023-05-10 12:09:22 +0200

    addgear styling

--- client/src/Pages/AddGear.tsx
+++ client/src/Pages/AddGear.tsx
@@ -30,14 +30,6 @@ const AddGear: React.FC = (): JSX.Element => {
     });
   };
 
-  // const handleSubmit = async () => {
-
-  // };
-
-  useEffect(() => {
-    console.log('🍻 AddGear component loggedInUser=', userInfo);
-  });
-
   async function handleSubmit() {
     try {
       gearJustAddedInfo = await supabase.addGear(
@@ -47,27 +39,19 @@ const AddGear: React.FC = (): JSX.Element => {
         formState.priceday,
         formState.deposit
       );
-      console.log('gearJustAddedInfo in handleSubmit', gearJustAddedInfo);
-      // console.log('gearJustAddedInfo in Upload', gearJustAddedInfo);
-
-      console.log('files', files);
       let fileArray: File[] = [];
       if (files) {
         fileArray = [...files];
       }
-      const fileUploads = fileArray.map(
-        (file) =>
-          supabase.uploadGear(
-            file,
-            userInfo?.session?.user.id + '/gear/' + gearJustAddedInfo[0].id
-          )
-        // supabase.uploadGear(file, userInfo?.session?.user.id)
+      const fileUploads = fileArray.map((file) =>
+        supabase.uploadGear(
+          file,
+          userInfo?.session?.user.id + '/gear/' + gearJustAddedInfo[0].id
+        )
       );
-      console.log('promises', fileUploads);
       await Promise.all(fileUploads);
-      console.log('all files uploaded');
     } catch (e) {
-      console.log('some files failed to upload', e);
+      console.log('Some files failed to upload', e);
     }
   }
   const navigate = useNavigate();
@@ -76,33 +60,33 @@ const AddGear: React.FC = (): JSX.Element => {
     <>
       <NavBar></NavBar>
       <form className='mx-12'>
-        <div className='space-y-12'>
-          <div className='border-b border-gray-900/10 pb-12'>
-            <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
-              <div className='sm:col-span-4'>
-                <label
-                  htmlFor='username'
-                  className='block text-sm font-medium leading-6 text-gray-900'
-                >
-                  Owner
-                </label>
-                <div className='mt-2'>
-                  <div className='flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md'>
-                    <span className='flex select-none items-center pl-3 text-gray-500 sm:text-sm'>
-                      gearhub.com/
-                    </span>
-                    <input
-                      type='text'
-                      name='username'
-                      id='username'
-                      autoComplete='username'
-                      className='block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6'
-                      placeholder='janesmith'
-                    />
+        <div className=''>
+          <div className=''>
+            <div className='grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+              <div className=''>
+                <div className='mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+                  <div className='sm:col-span-3'>
+                    <label
+                      htmlFor='country'
+                      className='block text-sm font-medium leading-6 text-gray-900'
+                    >
+                      Gear type
+                    </label>
+                    <div className='mt-2'>
+                      <select
+                        id='country'
+                        name='geartype'
+                        autoComplete='country-name'
+                        className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6'
+                      >
+                        <option>Bike</option>
+                        <option>SurfBoard</option>
+                        <option>Volleyball Net </option>
+                      </select>
+                    </div>
                   </div>
                 </div>
               </div>
-
               <div className='col-span-full'>
                 <label
                   htmlFor='about'
@@ -112,24 +96,22 @@ const AddGear: React.FC = (): JSX.Element => {
                 </label>
                 <div className='mt-2'>
                   <textarea
+                    placeholder='Example: 1985 Peugeot Carbolite 103, size 56cm, perfect for exploring the city or cruising'
                     value={formState.description}
                     onChange={handleChange}
                     id='about'
                     name='description'
                     rows={3}
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder: pl-1.5 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                   />
                 </div>
-                <p className='mt-3 text-sm leading-6 text-gray-600'>
-                  Write a few sentences about your gear.
-                </p>
               </div>
               <div className='col-span-full'>
                 <label
                   htmlFor='cover-photo'
                   className='block text-sm font-medium leading-6 text-gray-900'
                 >
-                  Cover photo
+                  Photos
                 </label>
                 <div className='mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10'>
                   <div className='text-center'>
@@ -175,34 +157,7 @@ const AddGear: React.FC = (): JSX.Element => {
             </div>
           </div>
 
-          <div className='border-b border-gray-900/10 pb-12'>
-            <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
-              <div className='sm:col-span-3'>
-                <label
-                  htmlFor='country'
-                  className='block text-sm font-medium leading-6 text-gray-900'
-                >
-                  Type of Gear
-                </label>
-                <div className='mt-2'>
-                  <select
-                    id='country'
-                    name='geartype'
-                    autoComplete='country-name'
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6'
-                  >
-                    <option>Bike</option>
-                    <option>SurfBoard</option>
-                    <option>Volleyball Net </option>
-                  </select>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='border-b border-gray-900/10 pb-12'>
-            <h2 className='text-base font-semibold leading-7 text-gray-900'>
-              Payment Information
-            </h2>
+          <div className='pb-12'>
             <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
               <div className='sm:col-span-3'>
                 <label
@@ -265,7 +220,7 @@ const AddGear: React.FC = (): JSX.Element => {
             </div>
           </div>
 
-          <div className='border-b border-gray-900/10 pb-12'>
+          {/* <div className='border-b border-gray-900/10 pb-12'>
             <h2 className='text-base font-semibold leading-7 text-gray-900'>
               Notifications
             </h2>
@@ -337,31 +292,32 @@ const AddGear: React.FC = (): JSX.Element => {
                 </div>
               </fieldset>
             </div>
-          </div>
+          </div> */}
         </div>
-
-        <div className='mt-6 flex items-center justify-end gap-x-6'>
-          <button
-            type='button'
-            onClick={() => navigate(`/myprofile`)}
-            className='text-sm font-semibold leading-6 text-gray-900'
-          >
-            Cancel
-          </button>
-          <button
-            type='submit'
-            onClick={(e) => {
-              e.preventDefault();
-              // submitting the text input to db
-              handleSubmit();
-              // uploading images to the bucket
-              // handleUpload();
-              navigate(`/mygear`);
-            }}
-            className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-          >
-            Save
-          </button>
+        <div className='pb-12'>
+          <div className='mt-6 flex items-center justify-end gap-x-6'>
+            <button
+              type='submit'
+              onClick={(e) => {
+                e.preventDefault();
+                // submitting the text input to db
+                handleSubmit();
+                // uploading images to the bucket
+                // handleUpload();
+                navigate(`/mygear`);
+              }}
+              className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+            >
+              List your gear on the site!
+            </button>
+            <button
+              type='button'
+              onClick={() => navigate(`/myprofile`)}
+              className='text-sm font-semibold leading-6 text-gray-900'
+            >
+              Cancel
+            </button>
+          </div>
         </div>
       </form>
     </>
