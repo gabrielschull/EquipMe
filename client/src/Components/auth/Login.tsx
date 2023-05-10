7584809c12a7eddb0f9e7b574d61602f35987d62
Raul Barros <raulhbarros@outlook.com>
2023-05-10 20:30:33 +0200

    some styling on login page

--- client/src/Components/auth/Login.tsx
+++ client/src/Components/auth/Login.tsx
@@ -1,119 +1,131 @@
 import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { useEffect, useContext } from 'react';
-
 import { supabaseClient } from '../../services/supabase.service';
 
-const Login: React.FC = (): JSX.Element => {
+const customTheme = {
+  default: {
+    colors: {
+      brand: 'hsl(153 60.0% 53.0%)',
+      brandAccent: 'hsl(154 54.8% 45.1%)',
+      brandButtonText: 'white',
+      // ..
+    },
+  },
+  dark: {
+    colors: {
+      brandButtonText: 'white',
+      defaultButtonBackground: '#2e2e2e',
+      defaultButtonBackgroundHover: '#3e3e3e',
+      //..
+    },
+  },
+  // You can also add more theme variations with different names.
+  evenDarker: {
+    colors: {
+      brandButtonText: 'white',
+      defaultButtonBackground: '#1e1e1e',
+      defaultButtonBackgroundHover: '#2e2e2e',
+      //..
+    },
+  },
+};
 
+const Login: React.FC = (): JSX.Element => {
   return (
     <>
-      <div className='component-container'>
-        <h2>Login</h2>
-      </div>
-
       <Auth
         supabaseClient={supabaseClient}
         appearance={{
-          className: {
-            container:
-              'flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8',
-            label: 'login-form-label',
-            button:
-              'flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
-            input:
-              'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: 'red',
+                brandAccent: 'darkred',
+              },
+            },
           },
         }}
+        providers={['google', 'facebook', 'twitter']}
+        // className: {
+        //   container:
+        //     'flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8',
+        //   label: 'login-form-label',
+        //   button:
+        //     'flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+        //   input:
+        //     'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+        // },
       />
 
-      {/*
-       This example requires updating your template:
+      {/* SIGN IN TO YOUR ACCOUNT COMPONENT STARTS HERE*/}
 
-       ```
-       <html class="h-full bg-white">
-       <body class="h-full">
-       ```
-     */}
-      <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
-        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-          <img
-            className='mx-auto h-10 w-auto'
-            src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
-            alt='Your Company'
-          />
-          <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
         </div>
 
-        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-          <form className='space-y-6' action='#' method='POST'>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
             <div>
               <label
-                htmlFor='email'
-                className='block text-sm font-medium leading-6 text-gray-900'
-              >
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
               </label>
-              <div className='mt-2'>
+              <div className="mt-2">
                 <input
-                  id='email'
-                  name='email'
-                  type='email'
-                  autoComplete='email'
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
                   required
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div>
-              <div className='flex items-center justify-between'>
+              <div className="flex items-center justify-between">
                 <label
-                  htmlFor='password'
-                  className='block text-sm font-medium leading-6 text-gray-900'
-                >
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900">
                   Password
                 </label>
-                <div className='text-sm'>
+                <div className="text-sm">
                   <a
-                    href='#'
-                    className='font-semibold text-indigo-600 hover:text-indigo-500'
-                  >
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500">
                     Forgot password?
                   </a>
                 </div>
               </div>
-              <div className='mt-2'>
+              <div className="mt-2">
                 <input
-                  id='password'
-                  name='password'
-                  type='password'
-                  autoComplete='current-password'
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
                   required
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div>
               <button
-                type='submit'
-                className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-              >
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 Sign in
               </button>
             </div>
           </form>
 
-          <p className='mt-10 text-center text-sm text-gray-500'>
-            Not a member?{' '}
-            <a
-              href='#'
-              className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
-            >
-              Start a 14 day free trial
-            </a>
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Sign in and find your gear{' '}
           </p>
         </div>
       </div>
