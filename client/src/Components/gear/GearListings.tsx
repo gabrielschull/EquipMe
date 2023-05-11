d151582b12d9eab5852fae9d5fcfcda0600fdf65
Ivan Afanasev <diykarelia@gmail.com>
2023-05-11 17:12:32 +0200

    stash

--- client/src/Components/gear/GearListings.tsx
+++ client/src/Components/gear/GearListings.tsx
@@ -109,9 +109,9 @@ const GearListings: React.FC = (): JSX.Element => {
             <form className="mt-10">
               <button
                 type="submit"
-                onClick={() => navigate(`/geardetails/${gear.id}`)}
+                onClick={() => navigate(`/geardetails/${gear.id}`, {state:{gear}})}
                 className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                See more details
+                See more details blah
               </button>
             </form>
           </div>
