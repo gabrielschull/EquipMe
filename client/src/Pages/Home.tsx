b995746587b0ec229233573b996e940a1772c038
jbratton12 <james.bratton11@gmail.com>
2023-05-11 10:17:44 +0200

    refactor landing page

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -5,6 +5,7 @@ import GearListings from '../Components/gear/GearListings';
 import AddGear from './AddGear';
 import Chat from '../Components/rentals/Chat';
 import Payment from '../Components/rentals/Payment';
+import GettingStarted from '../Components/auth/GettingStarted';
 // import EditUser from './EditUser';
 
 const Home: React.FC = (): JSX.Element => {
@@ -14,6 +15,7 @@ const Home: React.FC = (): JSX.Element => {
     <>
       <div className="component-container">
         <NavBar/>
+        <GettingStarted/>
         {/* <CurrentRental></CurrentRental> */}
         <Chat/>
         <GearListings/>
