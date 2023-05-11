41ec84a5b6d96add1235260101321de68f3fe2e0
Ivan Afanasev <diykarelia@gmail.com>
2023-05-11 13:39:48 +0200

    merging

--- client/src/Pages/GearDetails.tsx
+++ client/src/Pages/GearDetails.tsx
@@ -54,14 +54,12 @@ const GearDetails: React.FC = (): JSX.Element => {
   const [gearImages, setGearImages] = useState<any[]>([]);
   const userInfo = useSelector((state: RootState) => state.User);
 
-  const location = useLocation()
-  const gear = location.state?.gear
+  const location = useLocation();
+  const gear = location.state?.gear;
   const { id } = useParams();
-  console.log(gearInfo,"gearinfo")
-  
+  console.log(gearInfo, 'gearinfo');
 
-  console.log("OWNERID ==>", gear.owner_id)
-
+  console.log('OWNERID ==>', gear.owner_id);
 
   console.log('PARAMS ==> ', id);
 
@@ -80,84 +78,75 @@ const GearDetails: React.FC = (): JSX.Element => {
     }
   };
 
-  async function getGearImages () {
-    console.log(gear.owner_id,"OWOWOWOOWOWOWO")// CHECK HERE IN AM
+  async function getGearImages() {
+    console.log(gear.owner_id, 'OWOWOWOOWOWOWO'); // CHECK HERE IN AM
     try {
-    const { data, error } = await supabaseClient
-    .storage
-    .from('gearImagesBucket')
-    .list(`${gear.owner_id}/gear/${id}`, {
-      limit: 4,
-      offset: 0,
-      sortBy: { column: "name", order: "asc"}
-    })
+      const { data, error } = await supabaseClient.storage
+        .from('gearImagesBucket')
+        .list(`${gear.owner_id}/gear/${id}`, {
+          limit: 4,
+          offset: 0,
+          sortBy: { column: 'name', order: 'asc' },
+        });
 
-    console.log(`${gear.owner_id}/gear/${id}`)
+      console.log(`${gear.owner_id}/gear/${id}`);
 
-    if (error) console.log("ERROR IN IMAGE FETCH ==> ", error)
-    
-    if(data !== null){
-      console.log(data, "YARRRR")
-      setGearImages(data)
+      if (error) console.log('ERROR IN IMAGE FETCH ==> ', error);
 
+      if (data !== null) {
+        console.log(data, 'YARRRR');
+        setGearImages(data);
+      }
+    } catch (e: any) {
+      console.log(e, 'Error getting gear images');
     }
-  } catch (e: any){
-    console.log(e, "Error getting gear images")
-  }
   }
 
   useEffect(() => {
     handleSeeMoreDetails();
-    getGearImages()
+    getGearImages();
   }, []);
 
-  console.log(gearImages, "GEARIMAGES")
+  console.log(gearImages, 'GEARIMAGES');
 
-  gearImages.map((image) => (
-    console.log(image.name, "IMGNAME")))
+  gearImages.map((image) => console.log(image.name, 'IMGNAME'));
 
   return (
     <>
       <NavBar></NavBar>
       {gearInfo && (
-          {/* DO NOT TOUCH! This will later be turned into avail calendar
-          <Calendar /> */}
-        <div className="bg-white">
-          <div className="pt-6">
+        <div className='bg-white'>
+          <div className='pt-6'>
             <div>
-            {/* <nav aria-label="Breadcrumb"> //THIS IS TO BE COMMENTED BACK IN WHEN WE CREATE GEAR TYPES. DISPLAY GEAR TYPE INSTEAD OF BREADCRUMB.NAME
+              {/* <nav aria-label="Breadcrumb"> //THIS IS TO BE COMMENTED BACK IN WHEN WE CREATE GEAR TYPES. DISPLAY GEAR TYPE INSTEAD OF BREADCRUMB.NAME
               <ol
-                role='list'
-                className='mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8'
-              >
+                role="list"
+                className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
                 {product.breadcrumbs.map((breadcrumb) => (
                   <li key={breadcrumb.id}>
-                    <div className='flex items-center'>
+                    <div className="flex items-center">
                       <a
                         href={breadcrumb.href}
-                        className='mr-2 text-sm font-medium text-gray-900'
-                      >
+                        className="mr-2 text-sm font-medium text-gray-900">
                         {breadcrumb.name}
                       </a>
                       <svg
                         width={16}
                         height={20}
-                        viewBox='0 0 16 20'
-                        fill='currentColor'
-                        aria-hidden='true'
-                        className='h-5 w-4 text-gray-300'
-                      >
-                        <path d='M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z' />
+                        viewBox="0 0 16 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                        className="h-5 w-4 text-gray-300">
+                        <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
                       </svg>
                     </div>
                   </li>
                 ))}
-                <li className='text-sm'>
+                <li className="text-sm">
                   <a
                     href={product.href}
-                    aria-current='page'
-                    className='font-medium text-gray-500 hover:text-gray-600'
-                  >
+                    aria-current="page"
+                    className="font-medium text-gray-500 hover:text-gray-600">
                     {product.name}
                   </a>
                 </li>
@@ -166,17 +155,90 @@ const GearDetails: React.FC = (): JSX.Element => {
             </div>
 
             {/* Image gallery */}
+            <div
+              id='image-track'
+              style={{
+                display: 'flex',
+                gap: '4vmin',
+                position: 'absolute',
+                left: '50%',
+                top: '50%',
+                transform: 'translate(0%, -50%)',
+              }}
+            >
+              {gearImages.map((image, index) => {
+                return (
+                  <div
+                    key={image.name}
+                    className='aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block'
+                  >
+                    <img
+                      src={
+                        CDNURL +
+                        gear.owner_id +
+                        '/gear/' +
+                        id +
+                        '/' +
+                        image.name
+                      }
+                      alt=''
+                      className='h-full w-full object-cover object-center'
+                      style={{
+                        width: '40vmin',
+                        height: '56vmin',
+                        objectFit: 'cover',
+                        objectPosition: 'center',
+                      }}
+                    />
+                  </div>
+                );
+              })}
+            </div>
 
-            <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-  {gearImages.map((image, index) => {
+            {/* <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+            {gearImages.map((image, index) => {
+  if (gearImages.length === 1) {
+    return (
+      <div
+        key={image.name}
+        className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block"
+      >
+        <img
+          src={CDNURL + gear.owner_id + "/gear/" + id + "/" + image.name}
+          alt=""
+          className="h-full w-full object-cover object-center"
+          style={{objectFit: "contain"}}
+        />
+      </div>
+    );
+  } else if (gearImages.length === 2) {
+    return (
+      <div
+        key={image.name}
+        className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8"
+      >
+        <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+          <img
+            src={CDNURL + gear.owner_id + "/gear/" + id + "/" + image.name}
+            alt=""
+            className="h-full w-full object-cover object-center"
+            style={{objectFit: "contain"}}
+          />
+        </div>
+      </div>
+    );
+  } else if (gearImages.length >= 3) {
     if (index === 0) {
       return (
         <div
           key={image.name}
-          className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
+          className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block"
+        >
           <img
-            src={CDNURL + gear.owner_id + "/gear/" + id + '/' + image.name}
+            src={CDNURL + gear.owner_id + "/gear/" + id + "/" + image.name}
             alt=""
+            className="h-full w-full object-cover object-center"
+            style={{ objectFit: "contain" }}
           />
         </div>
       );
@@ -184,10 +246,11 @@ const GearDetails: React.FC = (): JSX.Element => {
       return (
         <div
           key={image.name}
-          className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+          className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8"
+        >
           <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
             <img
-              src={CDNURL + gear.owner_id + "/gear/" + id + '/' + image.name}
+              src={CDNURL + gear.owner_id + "/gear/" + id + "/" + image.name}
               alt=""
               className="h-full w-full object-cover object-center"
             />
@@ -198,17 +261,19 @@ const GearDetails: React.FC = (): JSX.Element => {
       return (
         <div
           key={image.name}
-          className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
+          className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg"
+        >
           <img
-            src={CDNURL + gear.owner_id + "/gear/" + id + '/' + image.name}
+            src={CDNURL + gear.owner_id + "/gear/" + id + "/" + image.name}
             alt=""
             className="h-full w-full object-cover object-center"
           />
         </div>
       );
     }
-  })}
-</div>
+  }
+})} 
+</div> */}
             {/* Product info */}
             <div className='mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16'>
               <div className='lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8'>
@@ -239,7 +304,7 @@ const GearDetails: React.FC = (): JSX.Element => {
                         <StarIcon
                           key={rating}
                           className={classNames(
-                            reviews.average > rating
+                            gearInfo.rating > rating
                               ? 'text-gray-900'
                               : 'text-gray-200',
                             'h-5 w-5 flex-shrink-0'
