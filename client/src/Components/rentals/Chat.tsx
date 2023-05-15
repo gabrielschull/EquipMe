1a4eba280e0f39ea754a35e7d984cf706884ba0b
Ivan Afanasev <diykarelia@gmail.com>
2023-05-15 19:32:55 +0200

    stash

--- client/src/Components/rentals/Chat.tsx
+++ client/src/Components/rentals/Chat.tsx
@@ -1,140 +1,137 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../Redux/store'
-import { supabase, supabaseClient } from "../../services/supabase.service";
-import { Root } from 'react-dom/client';
-import { addMessage, setMessages } from '../../Redux/MessageSlice';
+// import React, { useState, useRef, useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { AppDispatch, RootState } from '../../Redux/store'
+// import { supabase, supabaseClient } from "../../services/supabase.service";
+// import { Root } from 'react-dom/client';
+// import { addMessage, setMessages } from '../../Redux/MessageSlice';
 
 const Chat: React.FC = (): JSX.Element => {
-  const userInfo = useSelector((state: RootState) => state.User);
-  const [inputValue, setInputValue] = useState('');
-  const [isChatOpen, setIsChatOpen] = useState(false);
-  const messages = useSelector((state: RootState) => state.Message)
-  const dispatch = useDispatch()
+  //   const userInfo = useSelector((state: RootState) => state.User);
+  //   const [inputValue, setInputValue] = useState('');
+  //   const [isChatOpen, setIsChatOpen] = useState(false);
+  //   const messages = useSelector((state: RootState) => state.Message)
+  //   const dispatch = useDispatch()
 
-  const chatRef = useRef<HTMLDivElement>(null);
-  const buttonRef = useRef<HTMLButtonElement>(null);
+  //   const chatRef = useRef<HTMLDivElement>(null);
+  //   const buttonRef = useRef<HTMLButtonElement>(null);
 
-  console.log('userInfo!!! =>>>', userInfo)
+  //   console.log('userInfo!!! =>>>', userInfo)
 
-  
+  //   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //     e.preventDefault();
+  //     if (!inputValue) return;
+  //     dispatch(addMessage(inputValue))
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!inputValue) return;
-    dispatch(addMessage(inputValue))
+  //   }
 
-  }
+  //   const handleButtonClick = () => {
+  //     setIsChatOpen((prevState) => !prevState);
+  //   };
 
-  const handleButtonClick = () => {
-    setIsChatOpen((prevState) => !prevState);
-  };
+  //   const handleCloseClick = () => {
+  //     setIsChatOpen(false);
+  //   };
 
-  const handleCloseClick = () => {
-    setIsChatOpen(false);
-  };
+  // useEffect(() => {
 
+  //   async function getAllConversations() {
+  //     const { data: conversations, error } = await supabaseClient
+  //       .from('Conversations')
+  //       .select('id')
+  //       .or(`member1.eq.${userInfo.profile.id},member2.eq.${userInfo.profile.id}`);
 
-useEffect(() => {
+  //     if (error) {
+  //       console.error("Error fetching conversations: ", error);
+  //       return [];
+  //     }
 
-  async function getAllConversations() {
-    const { data: conversations, error } = await supabaseClient
-      .from('Conversations')
-      .select('id')
-      .or(`member1.eq.${userInfo.profile.id},member2.eq.${userInfo.profile.id}`);
-  
-    if (error) {
-      console.error("Error fetching conversations: ", error);
-      return [];
-    }
-  
-    return conversations;
-  }
+  //     return conversations;
+  //   }
 
-  async function getMessagesByConversation(conversationId: string) {
-    const { data: messages, error } = await supabaseClient
-      .from('Messages')
-      .select('*')
-      .eq('conversation_id', conversationId);
-  
-    if (error) {
-      console.error("Error fetching messages: ", error);
-      return [];
-    }
-  
-    return messages;
-  }
+  //   async function getMessagesByConversation(conversationId: string) {
+  //     const { data: messages, error } = await supabaseClient
+  //       .from('Messages')
+  //       .select('*')
+  //       .eq('conversation_id', conversationId);
 
-  getAllConversations()
-  
-}, userInfo.profile.id)
+  //     if (error) {
+  //       console.error("Error fetching messages: ", error);
+  //       return [];
+  //     }
 
+  //     return messages;
+  //   }
+
+  //   getAllConversations()
+
+  // }, userInfo.profile.id)
 
   return (
-    <div className='flex'>
-      <button
-        ref={buttonRef}
-        onClick={handleButtonClick}
-        className='fixed bottom-4 right-4 bg-blue-500 text-white rounded-full w-12 h-12 flex items-center justify-center focus:outline-none'
-      >
-        {isChatOpen ? '-' : '+'}
-      </button>
-      {isChatOpen && (
-        <div
-          ref={chatRef}
-          className='fixed bottom-16 right-4 bg-gray-100 h-96 w-64 rounded-lg mx-8 break-all flex flex-col'
-        >
-          
-          <button
-            onClick={handleCloseClick}
-            style={{
-              color: "darkgrey",
-              border: "none", 
-              borderRadius: "4px", 
-              width: "1.3rem", 
-              height: "1.3rem", 
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              outline: "none",
-              fontWeight: "900",
-              transition: "box-shadow 0.3s",
-            }}
-            className='absolute top-2 right-2 text-white'
-          >
-            X
-          </button>
-          {/* <div className='flex-grow overflow-y-auto p-4'>
-            {messages?.map((message) => (
-              <div
-                key={message.id}
-                className={`flex justify-${
-                  message.sender === 'user' ? 'end' : 'start'
-                } mb-2`}
-              >
-                <div
-                  className={`bg-blue-500 p-2 rounded-lg text-white max-w-xs ${
-                    message.sender === 'user' ? 'ml-2' : 'mr-2'
-                  }`}
-                >
-                  {message.content}
-                </div>
-              </div>
-            ))}
-          </div> */}
-          <form onSubmit={handleSubmit}>
-            <input
-              type='text'
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              className='border-2 border-gray-300 p-2 w-full rounded-lg focus:outline-none focus:border-blue-500 whitespace-normal overflow-wrap-normal'
-              placeholder='Type your message...'
-              style={{ flex: 'none' }}
-            />
-          </form>
-        </div>
-      )}
-    </div>
+    <h1>test</h1>
+    //     <div className='flex'>
+    //       <button
+    //         ref={buttonRef}
+    //         onClick={handleButtonClick}
+    //         className='fixed bottom-4 right-4 bg-blue-500 text-white rounded-full w-12 h-12 flex items-center justify-center focus:outline-none'
+    //       >
+    //         {isChatOpen ? '-' : '+'}
+    //       </button>
+    //       {isChatOpen && (
+    //         <div
+    //           ref={chatRef}
+    //           className='fixed bottom-16 right-4 bg-gray-100 h-96 w-64 rounded-lg mx-8 break-all flex flex-col'
+    //         >
+
+    //           <button
+    //             onClick={handleCloseClick}
+    //             style={{
+    //               color: "darkgrey",
+    //               border: "none",
+    //               borderRadius: "4px",
+    //               width: "1.3rem",
+    //               height: "1.3rem",
+    //               display: "flex",
+    //               alignItems: "center",
+    //               justifyContent: "center",
+    //               outline: "none",
+    //               fontWeight: "900",
+    //               transition: "box-shadow 0.3s",
+    //             }}
+    //             className='absolute top-2 right-2 text-white'
+    //           >
+    //             X
+    //           </button>
+    //           {/* <div className='flex-grow overflow-y-auto p-4'>
+    //             {messages?.map((message) => (
+    //               <div
+    //                 key={message.id}
+    //                 className={`flex justify-${
+    //                   message.sender === 'user' ? 'end' : 'start'
+    //                 } mb-2`}
+    //               >
+    //                 <div
+    //                   className={`bg-blue-500 p-2 rounded-lg text-white max-w-xs ${
+    //                     message.sender === 'user' ? 'ml-2' : 'mr-2'
+    //                   }`}
+    //                 >
+    //                   {message.content}
+    //                 </div>
+    //               </div>
+    //             ))}
+    //           </div> */}
+    //           <form onSubmit={handleSubmit}>
+    //             <input
+    //               type='text'
+    //               value={inputValue}
+    //               onChange={(e) => setInputValue(e.target.value)}
+    //               className='border-2 border-gray-300 p-2 w-full rounded-lg focus:outline-none focus:border-blue-500 whitespace-normal overflow-wrap-normal'
+    //               placeholder='Type your message...'
+    //               style={{ flex: 'none' }}
+    //             />
+    //           </form>
+    //         </div>
+    //       )}
+    //     </div>
   );
 };
 
