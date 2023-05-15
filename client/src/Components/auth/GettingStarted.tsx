0158f56d33d60b8205820392eb0617ba5eccd1d8
jbratton12 <james.bratton11@gmail.com>
2023-05-15 18:30:56 +0200

    styledhome

--- client/src/Components/auth/GettingStarted.tsx
+++ client/src/Components/auth/GettingStarted.tsx
@@ -15,6 +15,9 @@ import {
 } from '../../Redux/UserSlice';
 import { setFilteredGear } from '../../Redux/filteredGearSlice';
 import { updateLocation } from '../../Redux/UserSlice';
+import Background from "../Assets/Background.jpg"
+
+
 
 function onlyUnique(value: string, index: number, array: string[]) {
   return array.indexOf(value) === index;
@@ -28,6 +31,13 @@ const GettingStarted: React.FC = (): JSX.Element => {
   const [allGear, setAllGear] = useState<any[]>([]);
   const { profile } = useSelector((state: RootState) => state.User);
   const [center, setCenter] = useState({ lat: 0, lng: 0 });
+  const [backgroundIndex, setBackgroundIndex] = useState(0);
+  const backgroundImages = [
+    'Background.jpg',
+    'image2.jpg',
+    'image3.jpg',
+
+  ];
 
   const gearTypesArray = [
     '--------',
@@ -103,41 +113,15 @@ const GettingStarted: React.FC = (): JSX.Element => {
 
   return (
     <>
-      {/* <button
-        type="submit"
-        onClick={() => {
-          userInfo.profile.is_owner
-            ? supabase.updateIsOwnerToFalse(userInfo.profile.id)
-            : supabase.updateIsOwnerToTrue(userInfo.profile.id);
-          dispatch(toggleIsOwner(userInfo.profile.id));
-        }}
-        className="bg-white hover:bg-gray-100 text-black font-semibold py-2 px-3  rounded shadow">
-        {userInfo.profile.is_owner ? '✔' : ''} I want to lease my sports gear to
-        others
-      </button>
-      {userInfo.profile.is_owner && (
-        <button
-          type="button"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(`/addgear`);
-          }}
-          className="bg-white hover:bg-gray-100 text-black font-semibold py-2 px-3  rounded shadow">
-          Add Gear
-        </button>
-      )}
-      <button
-        type="submit"
-        onClick={() => {
-          userInfo.profile.is_renter
-            ? supabase.updateIsRenterToFalse(userInfo.profile.id)
-            : supabase.updateIsRenterToTrue(userInfo.profile.id);
-          dispatch(toggleIsRenter(userInfo.profile.id));
-        }}
-        className="bg-white hover:bg-gray-100 text-black font-semibold py-2 px-3  rounded shadow">
-        {userInfo.profile.is_renter ? '✔' : ''} I'm looking to rent some gear
-      </button> */}
-
+    <div
+    className="relative"
+    style={{
+      backgroundImage: `url(${Background})`,
+      backgroundSize: 'contain',
+      backgroundPosition: 'center',
+      backgroundAttachment: 'fixed',
+    }}
+  >
       <div className='flex items-center justify-center'>
         <div className='flex flex-col items-center space-y-12 mt-20 '>
           <button
@@ -282,47 +266,19 @@ const GettingStarted: React.FC = (): JSX.Element => {
               </>
             )}
           </Listbox>
-
-          {/* <div className="sm:col-span-3 mx-12">
-            <label
-              htmlFor="first-name"
-              className="block text-sm text-black font-semibold py-2 px-3">
-              Location
-            </label>
-            <div className="mt-2">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div> */}
-
-          {/* <div className="map-style"></div> */}
-          {/* <button
-            type="submit"
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mx-12"
-            onClick={(e) => {
-              e.preventDefault();
-              handleSearchButton();
-            }}>
-            Search
-          </button> */}
         </>
       )}
       <div className='flex justify-center items-center mt-10'>
         <button
           onClick={handleGeolocation}
           type='submit'
-          className='mt-10 flex items-center justify-center rounded-md border border-transparent bg-indigo-400 px-8 py-3 text-base font-medium text-white hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+          className='mt-10 mb-40 flex items-center justify-center rounded-md border border-transparent bg-indigo-400 px-8 py-3 text-base font-medium text-white hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
         >
           Update your location
         </button>
       </div>
-
-      {/* <MapContainer/> */}
+      </div>
+<MapContainer></MapContainer>
     </>
   );
 };
