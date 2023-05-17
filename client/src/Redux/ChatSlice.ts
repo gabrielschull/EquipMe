f4d147fa1914912bbbb8af5e7e72154c1963ae97
Gabriel <gschull991@gmail.com>
2023-05-17 11:20:49 +0200

    initiating convos works without refresh

--- client/src/Redux/ChatSlice.ts
+++ client/src/Redux/ChatSlice.ts
@@ -2,18 +2,26 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const ChatSlice = createSlice({
     name: 'chat',
-    initialState: {isOpen: false, currentConversationId: ''},
+    initialState: {isOpen: false, currentConversationId: '', previousConversations: []},
     reducers: {
         toggleChat: (state) => {
             state.isOpen = !state.isOpen
         },
         openChat: (state, action) => {
+            // if (state.currentConversationId) {
+            //     state.previousConversations.push(state.currentConversationId)
+            // }
             state.isOpen = true;
             state.currentConversationId = action.payload;
         },
         closeChat: (state) => {
             state.isOpen = false;
         },
+        // switchToPreviousChat: (state) => {
+        //     if (state.previousConversations.length > 0) {
+        //         state.currentConversationId = state.previousConversations.pop()
+        //     }
+        // },
     },
 });
 
@@ -21,5 +29,6 @@ export const {
     toggleChat,
     openChat,
     closeChat,
+    // switchToPreviousChat
 } = ChatSlice.actions;
 export default ChatSlice.reducer
